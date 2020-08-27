35c21724e65e2a2664531a40a42a6ff4441b5b33
shiyue <shiyuezju@gmail.com>
2020-08-27 13:30:23 +0800

    remove Functions code

--- Functions/scheduled_task/utils/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', ''),
-    'master_key': os.environ.get('ACCOUNT_KEY', ''),
-    'database_id': os.environ.get('COSMOS_DATABASE', ''),
-    'container_id': os.environ.get('COSMOS_CONTAINER', ''),
-}
