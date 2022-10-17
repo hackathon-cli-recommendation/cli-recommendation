478d402f223f854d833ef2984891eb51ce3a546e
Qinkai Wu <32201005+ReaNAiveD@users.noreply.github.com>
2022-10-17 17:25:52 +0800

    Update API/RecommendationService/offline_data_service.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- API/RecommendationService/offline_data_service.py
+++ API/RecommendationService/offline_data_service.py
@@ -23,7 +23,7 @@ async def get_recommend_from_offline_data(command_list, recommend_type, error_in
         result_future = loop.run_in_executor(None, get_recommend_from_cosmos, database, commands[-1:], recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num)
 
         result_2 = await result_2_future
-        if len(result_2) < top_num:
+        if len(result_2) >= top_num:
             return result_2
         else:
             return result_2 + await result_future
