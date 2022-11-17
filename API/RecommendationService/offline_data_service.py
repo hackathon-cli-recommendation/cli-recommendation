c372db3423da1acd4f2719c1bf8ea79d37dc85d0
Qinkai Wu <t-qinkaiwu@microsoft.com>
2022-11-17 17:19:51 +0800

    sharing connection

--- API/RecommendationService/offline_data_service.py
+++ API/RecommendationService/offline_data_service.py
@@ -1,8 +1,8 @@
 import asyncio
 import os
 
-from azure.cosmos import CosmosClient, PartitionKey
-from .util import get_latest_cmd, generated_query_kql, RecommendationSource, RecommendType, generated_cosmos_type, CosmosType
+from .cosmos_helper import query_recommendation_from_offline_data, query_recommendation_from_offline_data_2
+from .util import get_latest_cmd, RecommendationSource, RecommendType, generated_cosmos_type, CosmosType
 
 
 async def get_recommend_from_offline_data(command_list, recommend_type, error_info, top_num=50):
@@ -10,17 +10,15 @@ async def get_recommend_from_offline_data(command_list, recommend_type, error_in
     cosmos_type = generated_cosmos_type(recommend_type, error_info)
     commands = get_latest_cmd(command_list, 2)
 
-    client = CosmosClient(os.environ["CosmosDB_Endpoint"], os.environ["CosmosDB_Key"])
-    database = client.get_database_client(os.environ["CosmosDB_DataBase"])
     totalcount_threshold = int(os.environ["Solution_TotalCount_Threshold"]) if cosmos_type == CosmosType.Solution else int(os.environ["Command_TotalCount_Threshold"])
     ratio_threshold = int(os.environ["Solution_Ratio_Threshold"]) if cosmos_type == CosmosType.Solution else int(os.environ["Command_Ratio_Threshold"])
 
     if cosmos_type == CosmosType.Solution:
-        return get_recommend_from_cosmos(database, commands[-1:], recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num)
+        return get_recommend_from_cosmos(commands[-1:], recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num)
     else:
         # The recommended content matching the last two commands is preferred. If there is no data, it will fall back to the situation of matching the last command
-        result_2_future = loop.run_in_executor(None, get_recommend_from_cosmos, database, commands[-2:], recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num)
-        result_future = loop.run_in_executor(None, get_recommend_from_cosmos, database, commands[-1:], recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num)
+        result_2_future = loop.run_in_executor(None, get_recommend_from_cosmos, commands[-2:], recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num)
+        result_future = loop.run_in_executor(None, get_recommend_from_cosmos, commands[-1:], recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num)
 
         result_2 = await result_2_future
         if len(result_2) >= top_num:
@@ -29,17 +27,12 @@ async def get_recommend_from_offline_data(command_list, recommend_type, error_in
             return result_2 + await result_future
 
 
-def get_recommend_from_cosmos(database, commands, recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num=50):
+def get_recommend_from_cosmos(commands, recommend_type, error_info, totalcount_threshold, ratio_threshold, top_num=50):
     if len(commands) == 2:
-        recommendation_container = database.get_container_client(os.environ["Recommendation_Container_2"])
-        command_str = commands[-2] + "|" + commands[-1]
+        query_items = list(query_recommendation_from_offline_data_2(commands[-2], commands[-1], recommend_type, error_info))
     else:
-        recommendation_container = database.get_container_client(os.environ["Recommendation_Container"])
-        command_str = commands[-1]
+        query_items = list(query_recommendation_from_offline_data(commands[-1], recommend_type, error_info))
 
-    query = generated_query_kql(command_str, recommend_type, error_info)
-
-    query_items = list(recommendation_container.query_items(query=query, enable_cross_partition_query=True))
     result = []
     for item in query_items:
         if item['totalCount'] < totalcount_threshold:
