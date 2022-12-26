dd6853844fffc440faa8fdd7486e38da8c2edd71
Xing Zhou <Zhou.Xing@microsoft.com>
2022-12-26 16:48:41 +0800

    Update API/RecommendationService/util.py

--- API/RecommendationService/util.py
+++ API/RecommendationService/util.py
@@ -24,7 +24,7 @@ class RecommendationSource(int, Enum):
 class ScenarioSourceType(int, Enum):
     SAMPLE_REPO = 1
     DOC_CRAWLER = 2
-    KNOWLEDGE_BASE = 3
+    MANUAL_INPUT = 3
 
 
 def get_cosmos_type(recommend_type):
