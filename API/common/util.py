e78d8d54d4548ee3bef3880b0c7f6497a0400bfc
ZelinWang <zelinwang@microsoft.com>
2023-09-25 11:37:55 +0800

    update

--- API/common/util.py
+++ API/common/util.py
@@ -1,9 +1,18 @@
-from enum import Enum
-import os
+import base64
 import json
-import azure.functions as func
+import logging
+import os
+from enum import Enum
+from functools import wraps
 
+import azure.functions as func
+import jwt
+import requests
+from azure.functions import HttpRequest, HttpResponse
 from common.exception import ParameterException
+from jwt.algorithms import RSAAlgorithm
+
+logger = logging.getLogger(__name__)
 
 
 class ScenarioSourceType(int, Enum):
@@ -75,3 +84,55 @@ def generate_response(data, status, error=None):
         'api_version': os.environ["API_Version"]
     }
     return json.dumps(response_data)
+
+
+def verify_token(func):
+    @wraps(func)
+    def wrapper(req: HttpRequest) -> HttpResponse:
+        token = req.headers.get('Authorization')
+        if not token:
+            return HttpResponse("Authorization token is missing", status_code=401)
+        token = token.replace('Bearer ', '')
+        parts = token.split(".")
+        header = json.loads(base64.b64decode(parts[0] +"==").decode("utf-8")) 
+        payload = json.loads(base64.b64decode(parts[1] +"==").decode("utf-8"))
+        if 'alg' not in header or 'kid' not in header or 'azp' not in payload or 'aud' not in payload:
+            print("Token is invalid.")
+            return HttpResponse("Token is invalid", status_code=401)
+        alg = header['alg']
+        kid = header['kid']
+        azp = payload['azp']
+        aud = payload['aud']
+        if azp not in os.environ.get("ALLOWED_APP_IDS", "40b5afcb-b0db-4d2c-83a0-083dbb8e0c14,19ab3e9c-cfdb-4d6e-8e4f-342ef4f9e97d,a942385d-6d3c-415b-bfbb-7f4350377466").split(','):
+            logger.error("App ID is invalid.")
+            return HttpResponse("App ID is invalid", status_code=401)
+
+        tenant_id = os.environ.get("MICROSOFT_TENANT_ID", "72f988bf-86f1-41af-91ab-2d7cd011db47")
+        jwks_url = f"https://login.microsoftonline.com/{tenant_id}/discovery/v2.0/keys"
+
+        response = requests.get(jwks_url)
+        if response.status_code == 200:
+            jwks = response.json()
+        else:
+            logger.error("Failed to retrieve JWKS:", response.status_code, response.text)
+            return HttpResponse("Failed to retrieve JWKS", status_code=401)
+
+        # Load AAD's public key from the retrieved key set
+        public_key = None
+        for key in jwks["keys"]:
+            if key["kid"] == kid:
+                public_key = RSAAlgorithm.from_jwk(json.dumps(key))
+                break
+        if public_key is None:
+            logger.error("Failed to retrieve public key from JWKS")
+            return HttpResponse("Failed to retrieve public key from JWKS", status_code=401)
+
+        # Verify the JWT token: expiration, signature
+        try:
+            jwt.decode(token, public_key, algorithms=[alg], audience=aud)
+            logger.info("token is valid")
+        except jwt.InvalidTokenError as e:
+            logger.error(f"Token validation failed: {e}")
+            return HttpResponse(f"Token validation failed: {e}", status_code=401)
+        return func(req)
+    return wrapper
