764b4edb37363dffdaa460ba7b2158143e090d0b
qinkaiwu <qinkaiwu@microsoft.com>
2023-11-20 16:46:59 +0800

    Update

--- API/CopilotService/__init__.py
+++ API/CopilotService/__init__.py
@@ -178,6 +178,7 @@ def _find_most_relevant_command_chunk(context, question, chunks):
     parts = relevant_result.rsplit('||', 1)
     if len(parts) == 2:
         chunk_desc, score = parts
+        chunk_desc = chunk_desc.strip('"')
         if int(score) >= int(os.environ.get("RELEVANT_SCORE_THRESHOLD", "8")):
             try:
                 idx = [chunk["summary"] for chunk in chunks].index(chunk_desc)
